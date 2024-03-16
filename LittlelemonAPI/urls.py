6edcc4678d00898f54cfa866759ece71d069abec
Hanifi Demir <hanifidemir.sftwr@gmail.com>
2024-03-16 21:30:56 +0300

    url changes

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 #    path('ratings', views.RatingsView.as_view()),
+    path('djoser-token/',include('djoser.urls.authtoken')),
     path('api-auth/', include('rest_framework.urls')),
-    path('',include('djoser.urls')),
-    path('menu-items/', views.MenuItemsView.as_view()),
+    path('', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemsView.as_view()),
     path('categories/', views.CategoryView.as_view()),
     path('categories/<int:pk>', views.SingleCategoryView.as_view()),
@@ -16,5 +16,4 @@ urlpatterns = [
     path('cart/menu-items', views.CartView.as_view()),
     path('orders', views.OrderView.as_view()),
     path('orders/<int:pk>', views.OrderUpdateView.as_view()),
-
 ]
